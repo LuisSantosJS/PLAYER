79eced19f7a4ec8b52d2d9e4731966d168754d06
luissantosjs <dasilvasantosluisfelipe@gmail.com>
2022-03-13 01:50:56 -0500

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+      <title>Player de Vídeo</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+        <Script src="/scripts/script.js"></Script>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
