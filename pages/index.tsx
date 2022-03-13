dbeb10f407e62bf1e725316c295aec40675ce736
luissantosjs <dasilvasantosluisfelipe@gmail.com>
2022-03-13 12:03:52 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
       <div className="container">
         <Player
           textAdvertisement="Will fresco"
-          thumbnail="https://source.unsplash.com/random"
+          thumbnail=" https://source.unsplash.com/800x300"
           src="https://storage.googleapis.com/media-session/caminandes/short.mp4#t=1"
        // src="http://127.0.0.1:5500/videoplayback.mp4"
         />
